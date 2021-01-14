14d97e4160af2b8341aa06da054e5098e40421cd
empeekdev <dev@empeek.com>
2021-01-14 18:53:44 +0200

    Finished selenium script

--- tests.py
+++ tests.py
@@ -17,6 +17,40 @@ class Test():
   def teardown_method(self, method):
     self.driver.quit()
   
+  def test_register_an_account(self):
+    """Register new user account""" 
+    self.driver.get("http://openncart.herokuapp.com/")
+    self.driver.find_element_by_css_selector('#top-links > ul > li.dropdown > a > span.hidden-xs.hidden-sm.hidden-md').click()
+    time.sleep(2)
+    self.driver.find_element_by_css_selector('#top-links > ul > li.dropdown.open > ul > li:nth-child(1) > a').click()
+    time.sleep(2)
+    self.driver.find_element(By.NAME, "firstname").send_keys("Yura")
+    self.driver.find_element(By.NAME, "lastname").send_keys("Paneyko")
+    self.driver.find_element(By.NAME, "email").send_keys("yura@gmail.com")
+    self.driver.find_element(By.ID, "input-telephone").send_keys("0630275662")
+    self.driver.find_element(By.NAME, "address_1").send_keys("lviv")
+    self.driver.find_element(By.NAME, "city").send_keys("lviv")
+    self.driver.find_element(By.NAME, "postcode").send_keys("79019")
+    # self.driver.find_element(By.NAME, "country_id").send_keys("macbook")
+    self.driver.find_element(By.ID, "input-zone").click()
+    dropdown = self.driver.find_element(By.ID, "input-zone")
+    dropdown.find_element(By.XPATH, "//option[. = 'Anglesey']").click()
+    self.driver.find_element(By.ID, "input-password").send_keys("password_123")
+    self.driver.find_element(By.ID, "input-confirm").send_keys("password_123")
+    self.driver.find_element_by_css_selector('#content > form > div > div > input[type=checkbox]:nth-child(2)').click()
+    self.driver.find_element_by_css_selector('#content > form > div > div > input.btn.btn-primary').click()
+
+  def test_log_in(self):
+    """ Log into your account """
+    self.driver.get("http://openncart.herokuapp.com/")
+    self.driver.find_element_by_css_selector('#top-links > ul > li.dropdown > a > span.hidden-xs.hidden-sm.hidden-md').click()
+    time.sleep(2)
+    self.driver.find_element_by_css_selector('#top-links > ul > li.dropdown.open > ul > li:nth-child(2) > a').click()
+    time.sleep(1)
+    self.driver.find_element(By.NAME, "email").send_keys("yura@gmail.com")
+    self.driver.find_element(By.NAME, "password").send_keys("password_123")
+    self.driver.find_element_by_css_selector("#content > div > div:nth-child(2) > div > form > input").click()
+    
   def test_add_to_cart(self):
     """Add 15 macbooks pro to cart"""
 
@@ -32,13 +66,9 @@ class Test():
     self.driver.find_element(By.NAME, "quantity").send_keys("15")
     self.driver.find_element(By.ID, "button-cart").click()
     
-    time.sleep(2)
+    # time.sleep(15)
   
-  # def test_checkout(self):
-  #   """Buy ordered macbooks""" 
-
-  #   self.driver.find_element_by_css_selector('#cart > button').click()
-  #   self.driver.find_element_by_css_selector('#cart > ul > li:nth-child(2) > div > p > a:nth-child(2) > strong').click()
-    
-  #   time.sleep(10)
+  
+  
+  
   
